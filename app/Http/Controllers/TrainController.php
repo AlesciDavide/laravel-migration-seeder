e98fa293ca164ddc4ba5a54c2caf418d693a7037
AlesciDavide <davi.alesci@Gmail.com>
2024-07-18 15:24:27 +0200

    fine esercizio

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -10,7 +10,8 @@ class TrainController extends Controller
 {
     public function index(){
 
-        $trains = Train::all();
+        $date = '2024-07-18';
+        $trains = Train::whereDate('orario_di_partenza', $date)->get();
 
         return view('pages.index', compact('trains'));
     }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,6 +3,27 @@
 
     @section('main-content')
 
-    <h1>Ciao, qua dentro inserisci il contenuto del main con yield</h1>
+    @dump($trains);
+
+    <h1>Ecco i treni in partenza oggi!!</h1>
+    <section class="container">
+        @foreach ($trains as $train)
+
+        <article class="singleTrain">
+                <h1>
+                    Azienda: {{$train->azienda}}
+                </h1>
+                <h3>
+                    Stazione di partenza: {{$train->stazione_di_partenza}}
+                </h3>
+                <div>
+                    <p>Orario di partenza: {{$train->orario_di_partenza}}</p>
+                    <p>Orario di arrivo: {{$train->orario_di_arrivo}}</p>
+                    <p>Codice Treno: {{$train->codice_treno}} </p>
+                </div>
+        </article>
+
+        @endforeach
+    </section>
 
     @endsection
