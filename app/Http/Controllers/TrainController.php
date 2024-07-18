1639a6fa8d857d5c3a3dd80c658920aff669bf03
AlesciDavide <davi.alesci@Gmail.com>
2024-07-18 15:01:58 +0200

    create le rotte per l'index

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class TrainController extends Controller
 {
-    //
+    public function index(){
+
+        $trains = Train::all();
+
+        return view('pages.index', compact('trains'));
+    }
 
 }
